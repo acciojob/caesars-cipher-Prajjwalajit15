3f3174ebf6c18b04c1f4b9b06afccbcd1d1320bc
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-08 17:20:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -32,15 +30,29 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = []; // Store the decoded characters here
 
-  return; //return decodedArr
+  for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+
+    // Check if the character is in the lookup table
+    if (lookup.hasOwnProperty(char)) {
+      // If it is, add the corresponding character from the lookup table to the decodedArr
+      decodedArr.push(lookup[char]);
+    } else {
+      // If it's not in the lookup table, it's a non-alphabetic character, so add it unchanged
+      decodedArr.push(char);
+    }
+  }
+
+  // Convert the array of characters back to a string
+  const decodedStr = decodedArr.join('');
+  
+  return decodedStr;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+// You can test your code by running the above function and printing it to the console like this:
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
